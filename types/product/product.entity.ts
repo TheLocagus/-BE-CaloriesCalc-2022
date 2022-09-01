91db956037878c46c733c858f1457d9cf2a9ff4f
TheLocagus <locagus@o2.pl>
2022-09-01 21:50:28 +0200

    First commit as Nest App. Version 2.0.0

--- types/product/product.entity.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductEntity {
-    id?: string;
-    name: string;
-    proteins: number;
-    carbohydrates: number;
-    fats: number;
-    calories: number;
-    amount: number;
-}
